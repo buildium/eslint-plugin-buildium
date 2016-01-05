b76e7c374292dca67287742030e78476c9213c3f
Jeff Balboni <jbalboni@gmail.com>
2016-01-05 16:26:07 -0500

    Added rule to catch angular-bsfy

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ module.exports = {
         'func-names': require('./rules/func-names/func-names'),
         'template-location': require('./rules/template-location/template-location'),
         'imports-lodash': require('./rules/imports/imports')(/^lodash$/i, 'Use underscore or import specific lodash functions instead of requiring all of it'),
-        'imports-jquery': require('./rules/imports/imports')(/^jQuery$/, 'Use jquery, not jQuery (this will cause extra code to be bundled)')
+        'imports-jquery': require('./rules/imports/imports')(/^jQuery$/, 'Use jquery, not jQuery (this will cause extra code to be bundled)'),
+        'imports-angular-bsfy': require('./rules/imports/imports')(/^angular-bsfy/, 'We no longer use angular-bsfy, use angular or an angular- package')
     },
     rulesConfig: {
         'func-names': 2,
         'template-location': 2,
         'imports-lodash': 2,
-        'imports-jquery': 2
+        'imports-jquery': 2,
+        'imports-angular-bsfy': 2
     }
 };
