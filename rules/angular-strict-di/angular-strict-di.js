94c6bcf8cbd961e500c3845a29ca46a8649a2cf5
Calvin <calvin.mceachron@buildium.com>
2019-06-17 14:18:47 -0400

    add lint rule for dependency injection with angular

---
+++ rules/angular-strict-di/angular-strict-di.js
@@ -0,0 +1,99 @@
+module.exports = {
+    meta: {
+        type: 'problem',
+        docs: {
+            description: 'disallow functions or providers that have not been explicitly annotated',
+            category: 'Possible Errors',
+        },
+        fixable: 'code',
+        schema: [],
+    },
+    create: function(context) {
+        const diNodes = [];
+        const injectArraysByFunction = {};
+
+        function report(node) {
+            context.report(node, '{{nodeName}} is not using explicit annotation and cannot be invoked in strict mode', {
+                nodeName: node.id.name
+            });
+        }
+
+        function hasNgAnnotateComment(node) {
+            const sourceCode = context.getSourceCode();
+            const comments = sourceCode.getAllComments();
+            const annotateComments = comments.filter(comment => comment.value.trim() === '@ngInject');
+
+            const hasComment = annotateComments.some(comment => {
+                const tokensBetween = sourceCode.getTokensBetween(comment, node);
+                const commentBelongsToAnother = tokensBetween.some(token => token.type === 'Keyword' && ['function', 'class'].includes(token.value));
+                return !commentBelongsToAnother;
+            });
+
+            return hasComment;
+        }
+
+        function isAngularInjectable(param) {
+            return param.startsWith('$');
+        }
+
+        function isCustomInjectable(param) {
+            return /^[A-Z]/.test(param);
+        }
+
+        function isAngularFunctionOrProvider(node) {
+            if (!node.params) { return false; }
+            const params = node.params.map(param => param.name);
+            return params.some(isAngularInjectable) 
+                || params.some(isCustomInjectable);
+        }
+
+        function injectArrayExistsForNode(node) {
+            const functionName = node.id.name;
+            const injectArray = injectArraysByFunction[functionName];
+
+            if (!injectArray) { return false; }
+            
+            const params = node.params.map(param => param.name);
+            if (!(injectArray.length === params.length)) { return false; }
+            
+            const paramsAreMatching = params.every((param, index) => param === injectArray[index]);
+            if (!paramsAreMatching) { return false; }
+            
+            return true;
+        }
+
+        function checkNodes() {
+            diNodes.forEach(node => {
+                if (!injectArrayExistsForNode(node)) {
+                    report(node);
+                }
+            });
+        }
+
+        function checkDependencyInjection(node) {
+            const requiresAnnotation = hasNgAnnotateComment(node) || isAngularFunctionOrProvider(node);
+            if (!requiresAnnotation) { return; }
+
+            diNodes.push(node);
+        }
+
+        return {
+            FunctionDeclaration: checkDependencyInjection,
+            FunctionExpression: checkDependencyInjection,
+            AssignmentExpression: function(node) {
+                function isInjectArray(node) {
+                    return node.left.property.name === '$inject'
+                        && node.right.type === 'ArrayExpression';
+                }
+
+                if (isInjectArray(node)) {
+                    const functionName = node.left.object.name;
+                    injectArraysByFunction[functionName] = node.right.elements.map(element => element.value);
+                }
+            },
+            "Program:exit": function() {
+                checkNodes();
+            },
+        }
+    }
+};
---
+++ rules/angular-strict-di/angular-strict-di.test.js
@@ -0,0 +1,90 @@
+const { RuleTester } = require('eslint');
+const rule = require('./angular-strict-di');
+
+new RuleTester({
+    parserOptions: { 
+        ecmaVersion: 6,
+        sourceType: 'module',
+    }
+}).run('angular-strict-di', rule, {
+    invalid: [
+        {
+            code: `
+            export function InvalidControllerNoInject(MyService) {}
+            `,
+            errors: [
+                { message: new RegExp('is not using explicit annotation and cannot be invoked in strict mode') }
+            ]
+        },
+        {
+            code: `
+            // @ngInject
+            export function InvalidControllerInjectComment() {}
+            `,
+            errors: [
+                { message: new RegExp('is not using explicit annotation and cannot be invoked in strict mode') }
+            ]
+        },
+        {
+            code: `
+            export function InvalidControllerInjectMismatch(MyService) {};
+            InvalidControllerInjectMismatch.$inject = ['hello', 'world'];
+            `,
+            errors: [
+                { message: new RegExp('is not using explicit annotation and cannot be invoked in strict mode') }
+            ]
+        },
+        {
+            code: `
+            export function InvalidControllerInjectMismatchExtra(MyService) {};
+            InvalidControllerInjectMismatchExtra.$inject = ['MyService', 'hello', 'world'];
+            `,
+            errors: [
+                { message: new RegExp('is not using explicit annotation and cannot be invoked in strict mode') }
+            ]
+        },
+        {
+            code: `
+            const component = {};
+            component.controller = function InvalidComponentController($q) {
+
+            };
+            export default component;
+            `,
+            errors: [
+                { message: new RegExp('is not using explicit annotation and cannot be invoked in strict mode') }
+            ]
+        }
+    ],
+
+    valid: [
+        {
+            code: `
+                export function ValidControllerNoInjectables() {}
+            `,
+        },
+        {
+            code: `
+                export function ValidControllerWithInjectable(MyService) {
+                };
+                ValidControllerWithInjectable.$inject = ['MyService'];
+            `,
+        },
+        {
+            code: `
+                ValidControllerWithInjectableBefore.$inject = ['MyService'];
+                export function ValidControllerWithInjectableBefore(MyService) {
+                };
+            `,
+        },
+        {
+            code: `
+                function validMethodNoInjectables(param1, param2) {}
+
+                export function ValidControllerWithInjectable(MyService) {
+                };
+                ValidControllerWithInjectable.$inject = ['MyService'];
+            `,
+        }
+    ],
+});
